0e0422e9280dec6f3a7af722db0232da12eec131
dtimonovskij <dtimonovskij@gmail.com>
2020-07-01 23:46:17 +0300

    fixed bag

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const toggle = document.getElementsByClassName("navbar-toggle");
+
+toggle[0].addEventListener("click", function(event) {
+  event.preventDefault();
+  this.classList.toggle("is-active");
+});
+
+const closeNavbarMobile = document.getElementsByClassName("close");
+
+closeNavbarMobile[0].addEventListener("click", function(event) {
+  event.preventDefault();
+  toggle[0].classList.remove("is-active");
+});
 $(document).ready(function(){
     $('.slider').slick();
 });
